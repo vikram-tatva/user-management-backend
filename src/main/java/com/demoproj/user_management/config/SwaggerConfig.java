9641321f15fd54e316b150a237dadc3fa49215d8
vikram-tatva <vikram.dharajiya@tatvasoft.com>
2025-12-31 12:31:54 +0530

    Configuration jwt with swagger ui

---
+++ src/main/java/com/demoproj/user_management/config/SwaggerConfig.java
@@ -0,0 +1,26 @@
+package com.demoproj.user_management.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI openAPI() {
+        SecurityScheme jwtScheme = new SecurityScheme()
+                .name("Authorization")
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT");
+
+        return new OpenAPI()
+                .components(new Components()
+                        .addSecuritySchemes("bearerAuth", jwtScheme))
+                .addSecurityItem(new SecurityRequirement()
+                        .addList("bearerAuth"));
+    }
+}
--- src/main/java/com/demoproj/user_management/filters/JwtAuthFilter.java
+++ src/main/java/com/demoproj/user_management/filters/JwtAuthFilter.java
@@ -45,4 +45,11 @@ public class JwtAuthFilter extends OncePerRequestFilter {
 
         filterChain.doFilter(request, response);
     }
+
+    @Override
+    protected boolean shouldNotFilter(HttpServletRequest request) {
+        String path = request.getServletPath();
+        return path.startsWith("/swagger-ui")
+                || path.startsWith("/v3/api-docs");
+    }
 }
