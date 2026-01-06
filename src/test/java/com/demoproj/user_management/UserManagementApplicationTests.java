2c433b56b5c02896d166567a2d5fa5a81ce0c411
vikram-tatva <vikram.dharajiya@tatvasoft.com>
2026-01-06 16:39:31 +0530

    Configure test environment for ci build with h2 database

--- src/test/java/com/demoproj/user_management/UserManagementApplicationTests.java
+++ src/test/java/com/demoproj/user_management/UserManagementApplicationTests.java
@@ -2,7 +2,9 @@ package com.demoproj.user_management;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class UserManagementApplicationTests {
 
